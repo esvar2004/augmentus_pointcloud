332d4da64f8f23265c60581b271e2096f91c4ef3
Esvaran Arun <esvar2004@gmail.com>
2026-07-02 14:52:02 +0530

    feat: Introduced data loader class to handle reading of point cloud + unit tests

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,18 @@
 """Main orchestrator file for handling end-to-end point cloud processing."""
 import open3d as o3d
+from data_loader import DataLoader
 
 class MainApp:
 	"""Main application entry point for point cloud processing."""
 
 	def __init__(self) -> None:
 		self.name = "MainApp"
+		self.data_loader = DataLoader()
 
 	def run(self) -> None:
-		"""Load a point cloud, and visualize it."""
-		point_cloud = o3d.data.EaglePointCloud()
-		pcd = o3d.io.read_point_cloud(point_cloud.path)
-		o3d.visualization.draw_geometries([pcd], window_name=f"{self.name}")
+		"""Loads a point cloud and visualizes it."""
+		pcd = self.data_loader.load()
+		o3d.visualization.draw_geometries([pcd], window_name=f"{self.name} - Before")
 
 if __name__ == "__main__":
 	app = MainApp()
